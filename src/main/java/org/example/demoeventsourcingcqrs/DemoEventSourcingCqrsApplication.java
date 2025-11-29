9f72b2956b80330de9b3bec90ad4fd21afdd7ab2
salmaabakil <153354773+salmaabakil@users.noreply.github.com>
2025-11-29 22:08:57 +0100

    Main

---
+++ src/main/java/org/example/demoeventsourcingcqrs/DemoEventSourcingCqrsApplication.java
@@ -0,0 +1,13 @@
+package org.example.demoeventsourcingcqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEventSourcingCqrsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoEventSourcingCqrsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/demoeventsourcingcqrs/DemoEventSourcingCqrsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demoeventsourcingcqrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoEventSourcingCqrsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
